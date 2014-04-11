47b4d3fe4d696b94eb192a7adf96b6729feb0099
Steve McMahon <steve@dcn.org>
2014-04-11 08:13:39 -0700

    Begin work on navicon

--- diazotheme/yoloelections/diazo_resources/static/main.js
+++ diazotheme/yoloelections/diazo_resources/static/main.js
@@ -3,4 +3,17 @@
 /*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
 /*global jQuery:false, document:false, window:false, location:false */
 
-console.log('main.js loaded');
+jQuery(function ($) {
+    var nav_viz = false;
+
+    $('#navicon').click(function () {
+        if (nav_viz) {
+            $('#portal-column-content').attr('class', 'cell width-16 position-0');
+            $('#portal-column-one').hide();
+        } else {
+            $('#portal-column-content').attr('class', 'cell width-12 position-5');
+            $('#portal-column-one').show();
+        }
+        nav_viz = ! nav_viz;
+    });
+});
