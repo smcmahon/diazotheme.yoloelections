7859f78b027a0ff8c9ae26610904025e257ebfaa
Steve McMahon <steve@dcn.org>
2014-07-21 17:05:24 -0700

    js for maps

--- diazotheme/yoloelections/diazo_resources/static/main.js
+++ diazotheme/yoloelections/diazo_resources/static/main.js
@@ -28,4 +28,51 @@ jQuery(function ($) {
         nav_viz = ! nav_viz;
 
     });
+
+    var currentImage;
+
+    $(".accordion").tabs(".accordion dd", {tabs: 'dt', effect: 'slide'}).addClass('active');
+
+    $('.section-archives area').removeAttr('title');
+
+    $('.section-archives map').after('<div class="tooltip">tooltip</div>');
+
+    $(".section-archives area").tooltip({
+        relative: false,
+        position: "center right",
+        opacity: 1.0,
+        tip:'.tooltip',
+        delay:0,
+        onBeforeShow: function(e) {
+            // fill the tooltip with data from the precinct breakdown
+            var i, row;
+            var trow = $(this.getTrigger().attr('href'));
+            var trc = trow.children().clone();
+            var thead = trow.prevAll('.btitle').first();
+            var thc = thead.children().clone();
+            var tip = this.getTip();
+            var table = $('<table />');
+
+            tip.empty().append(table);
+            for (i = 0; i < thc.length; i++) {
+                if (i != 1) {
+                    row = $('<tr />');
+                    if (i === 0) {
+                        row.addClass('ptitle');
+                    }
+                    row.append(thc[i]);
+                    row.append(trc[i]);
+                    table.append(row);
+                }
+            }
+
+        }
+        });
+
+
+    currentImage = $('img.pmap:first');
+    $('img.pmap').mouseenter(function(){
+        currentImage = this;
+    });
+
 });
